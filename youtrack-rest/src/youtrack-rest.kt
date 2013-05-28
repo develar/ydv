c0c43803ba3d9e35677efebd383756bcd813c4bf
develar <develar@gmail.com>
2013-05-28 21:10:56 +0200

    importer

--- youtrack-rest/src/youtrack-rest.kt
+++ youtrack-rest/src/youtrack-rest.kt
@@ -1,5 +1,15 @@
 package org.jetbrains.youtrackRest
 
-public class Youtrack {
+import html.XMLHttpRequest
 
+public class Youtrack {
+  public fun connect(host:String, login:String, password:String) {
+    val request = XMLHttpRequest()
+    request.setRequestHeader("Accept", "application/json")
+    request.open("POST", "$host/rest/user/login?login=$login&password=$password", false)
+    request.send()
+    if (request.response != null) {
+
+    }
+  }
 }
