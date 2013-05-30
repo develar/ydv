f99ba02f65218fbca08cbd5fb21cc80e49f3c1e6
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2013-05-30 09:43:33 +0200

    importer

--- importer/src/importer.kt
+++ importer/src/importer.kt
@@ -1,2 +1,28 @@
 package org.jetbrains.ydv
 
+import org.jetbrains.youtrackRest.Youtrack
+
+public native trait MessageEvent {
+  val data:Any
+}
+
+public native trait ImportMessage {
+  val host:String
+  val login:String
+  val password:String
+}
+
+public native var onmessage:((event:MessageEvent)->Unit)? = noImpl
+
+public native fun postMessage(message:Any?):Unit = noImpl
+public native fun importScripts(path:String):Unit = noImpl
+
+fun main(args:Array<String>) {
+  onmessage = {
+    val data = it.data as ImportMessage
+    val youtrack = Youtrack()
+    if (youtrack.connect(data.host, data.login, data.password)) {
+      postMessage("connected")
+    }
+  }
+}
--- youtrack-rest/src/youtrack-rest.kt
+++ youtrack-rest/src/youtrack-rest.kt
@@ -3,13 +3,15 @@ package org.jetbrains.youtrackRest
 import html.XMLHttpRequest
 
 public class Youtrack {
-  public fun connect(host:String, login:String, password:String) {
+  public fun connect(host:String, login:String, password:String):Boolean {
     val request = XMLHttpRequest()
+    request.open("POST", "$host/rest/user/login", false)
     request.setRequestHeader("Accept", "application/json")
-    request.open("POST", "$host/rest/user/login?login=$login&password=$password", false)
-    request.send()
+    request.send("login=$login&password=$password")
+    html.console.log(request.response!!)
     if (request.response != null) {
 
     }
+    return true
   }
 }
